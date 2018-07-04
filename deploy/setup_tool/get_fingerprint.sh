c317a8288bb4fd23d58673589954aace48dc24b4
lastsonic <yang.hu@cloudwiz.cn>
2018-07-04 18:10:20 +0800

    1st

---
+++ deploy/setup_tool/get_fingerprint.sh
@@ -0,0 +1,7 @@
+for line in `cat hosts | awk -F ' ' '{print $1}'`
+#for line in `cat hosts`
+        do
+                ssh-keyscan $line
+                #ssh-keyscan $line >> /home/centos/.ssh/known_hosts
+                ssh-keyscan $line >> /root/.ssh/known_hosts
+        done
